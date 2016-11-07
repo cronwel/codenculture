2894262254e31bdb42797230e8565614d8287ebe
Cronwel Noel Irias <noelirias@gmail.com>
2016-11-07 07:48:17 -0500

    Users db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105140514) do
+ActiveRecord::Schema.define(version: 20161107124541) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
