cd0bec3290408861b3fd2277c27ec629739f6f82
Cronwel Noel Irias <noelirias@gmail.com>
2016-11-09 11:47:54 -0500

    Uploading to Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Picture.create(title: 'apples', url: 'https://s-media-cache-ak0.pinimg.com/736x/93/5d/a2/935da22caf2096ffafd1499289912fca.jpg')
