fde3523b9db46450d6d0e7c525751beed31f62e1
Jenee Washington <jeneewashington@icloud.com>
2016-11-08 22:35:01 -0500

    Updated CSS for user sessions

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:email], params[:password])
-      redirect_back_or_to(:pictures, notice: 'Login successful')
+      redirect_back_or_to(:pictures, notice: 'You have made it to the other side...')
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
@@ -16,6 +16,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to(:users, notice: 'Logged out!')
+    redirect_to(:users, notice: 'Bye Felicia')
   end
 end
